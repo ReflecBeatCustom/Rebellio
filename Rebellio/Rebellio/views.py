c0f3217bbe864bedaa4ef6a65699125a85f68ff8
ilyee <493647673@qq.com>
2020-08-26 22:29:19 +0800

    update login

--- Rebellio/Rebellio/views.py
+++ Rebellio/Rebellio/views.py
@@ -30,8 +30,11 @@ def login(request):
             username = login_form.cleaned_data['username']
             password = login_form.cleaned_data['password']
             try:
-                results = models.Accounts.objects.raw("SELECT id FROM Accounts WHERE AccountName = '{0}' AND Passwd = PASSWORD('{1}')".format(username, password))
+                results = models.Accounts.objects.raw("SELECT AccountName, AccessLevel FROM Accounts WHERE AccountName = '{0}' AND Passwd = PASSWORD('{1}')".format(username, password))
                 if len(results) > 0:
+                    request.session['is_login'] = True
+                    request.session['user_name'] = results[0][0]
+                    request.session['user_access_level'] = int(results[0][1])
                     return redirect('/home')
                 else:
                     message = "密码不正确或用户不存在!"
